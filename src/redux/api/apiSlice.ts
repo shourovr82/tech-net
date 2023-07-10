b820b52214ce1b847e237fcf51650fbbb0572fa6
shourovr82 <shafinur512@gmail.com>
2023-07-10 23:51:20 +0600

    [DONE] redux rtk query added with optimistic update

---
+++ src/redux/api/apiSlice.ts
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const api = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
+  tagTypes: ['comments'],
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => '/products',
+    }),
+    getSingleProducts: builder.query({
+      query: (id) => `/product/${id}`,
+    }),
+    postComment: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/comment/${id}`,
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['comments'],
+    }),
+    getComment: builder.query({
+      query: (id) => `/comment/${id}`,
+      providesTags: ['comments'],
+    }),
+  }),
+});
+
+export const {
+  useGetProductsQuery,
+  useGetSingleProductsQuery,
+  usePostCommentMutation,
+  useGetCommentQuery,
+} = api;
